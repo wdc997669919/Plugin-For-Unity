47f0c703a90d340e526d7412be4405604a2b3adc
NightWater <838801978@qq.com>
2018-12-28 14:39:57 +0800

    学习了Dotween队列部分

--- DotweenDemo/Assets/Script/ScriptOne.cs
+++ DotweenDemo/Assets/Script/ScriptOne.cs
@@ -37,7 +37,7 @@ public class ScriptOne : MonoBehaviour {
         //第二个参数 duration：持续时间
         //第三个参数：震动次数（频率）
         //第四个参数：取值范围0-1   取值最大，往反方向的力越大  
-        //transform.DOPunchPosition(new Vector3(0,1,0),2,2,0.1f);
+        //transform.DOPunchPosition(new Vector3(0, 1, 0), 2, 2, 0.1f);
         //transform.DOPunchRotation(new Vector3(0, 1, 0), 2, 2, 0.1f);
         //transform.DOPunchScale(new Vector3(0, 1, 0), 2, 2, 0.1f);
 
@@ -105,8 +105,37 @@ public class ScriptOne : MonoBehaviour {
         #endregion
 
         #region 队列
-
+        //两种常用的初始化方法，建议使用第一种
+        Sequence sequence = DOTween.Sequence();
+        //DOTween.Sequence().Append();
+        //1.给队列添加方法
+        //添加到sequence的方法会依次执行，并且可以使用AppendInterval方法来延迟执行其后的方法
+        //sequence.Append(transform.DOMove(Vector3.one,2));   
+        //sequence.AppendInterval(1); //延时1秒
+        //sequence.Append(transform.DOMove(new Vector3(2,2,2), 2));
+        //2.给队列插入方法  注意：参数atPosition实际上指的是时间
+        //在某个时间点插入该方法，要注意这个时间点在当前队里中是否有方法，否则会覆盖其他的方法
+        //sequence.Insert(0,transform.DOMove(Vector3.one, 2));
+        //3.在队列加入方法，可以通过Insert或Join加入相关方法
+        //sequence.Append(transform.DOMove(Vector3.one, 2));
+        //sequence.Join(transform.DOScale(Vector3.one*2,2));  //这个方法会和上面的方法同时执行
+        //sequence.AppendInterval(1); //延时1秒
+        //sequence.Append(transform.DOMove(new Vector3(2, 2, 2), 2));
+        //4.在队列之前添加方法   预添加的方法里面后添加的先执行
+        // 比如下面fun2先执行  fun1后执行
+        //sequence.Prepend(transform.DOMove(Vector3.one, 2));//fun1
+        //sequence.Prepend(transform.DOMove(Vector3.one*2, 2));//fun2
+        //5.回调函数  在队列指定的时间进行函数的调用   预添加，队列追加，队列插入 三种模式都有回调方法
+        //sequence.InsertCallback(5, InsertCallFunction);
+        //sequence.AppendCallback(InsertCallFunction);
+        //sequence.PrependCallback(InsertCallFunction);
         #endregion
 
     }
+
+    public void InsertCallFunction()
+    {
+        Debug.Log("InsertCallFunction");
+    }
+
 }
